cc93572da0854f930ed99b5a1e094c8e8e13a393
MnguniMJ <mabalehamnguni@gmail.com>
2025-03-12 23:18:30 +0200

    expaining cashing in next

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -8,6 +8,12 @@ interface Users {
 
 const UsersPage = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    /* to cache data we ned to pass a second parameter (object)
+        e.g { cache: 'on-store'} to prevent next from storing your data. this is used where data keep changing
+           or { next: { revalidate: 10 } } getting fresh data every 10 sec
+
+           note: this cashing can only be used in fetch... axios does not have it yet
+    */
     const users: Users[] = await res.json();
 
   return (
