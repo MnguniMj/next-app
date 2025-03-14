b27c0fbf78a5218bad07598c8d58c9b23d9e9095
MnguniMJ <mabalehamnguni@gmail.com>
2025-03-14 08:37:17 +0200

    intro to tailwind

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import AddtoCard from './AddtoCard'
-import styles from "./Products.module.css"
 
 
 
 const Products = () => {
   return (
     <><h1>Products</h1>
-        <div className={styles.card}>
+        <div className="p-5 my-5 bg-sky-400 text-white text-xl rounded-md hover:bg-sky-500">
             <AddtoCard />
         </div>
     </>
