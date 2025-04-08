6c6450c7dca1353351f72be8035d96d2c54640ac
MnguniMJ <mabalehamnguni@gmail.com>
2025-04-08 23:21:33 +0200

    edit navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
-  return <div className="bg-sky-400 p-5 mb-5 ">Navbar</div>;
+  return (
+    <div className="flex bg-sky-400 p-5 mb-5 ">
+      <Link href="/">NextJs</Link>
+      <Link href="/users">Users</Link>
+    </div>
+  );
 };
 
 export default NavBar;
