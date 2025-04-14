f497a14be917de98c4e4b5a9c5ed344b77bee6db
MnguniMJ <mabalehamnguni@gmail.com>
2025-04-14 23:15:26 +0200

    CRUD on database

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,12 +1,12 @@
-import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import { prisma } from "@/prisma/client";
+import { error } from "console";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Ghuni" },
-    { id: 2, name: "John" },
-  ]);
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
@@ -17,5 +17,19 @@ export async function POST(request: NextRequest) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (user)
+    return NextResponse.json({ error: "User already exist" }, { status: 400 });
+
+  const newUser = await prisma.user.create({
+    data: {
+      email: body.email,
+      name: body.name,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 }
