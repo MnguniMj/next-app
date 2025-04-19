2af4d560131ddd5b20db7d67c634b693cfe20d31
MnguniMJ <mabalehamnguni@gmail.com>
2025-04-20 00:26:47 +0200

    using Cld upload Widget

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import Products from './components/Products'
+import Image from "next/image";
+import Link from "next/link";
+import Products from "./components/Products";
 
 export default function Home() {
   return (
@@ -9,5 +9,5 @@ export default function Home() {
       <Link href="/users">Users</Link>
       <Products />
     </main>
-  )
+  );
 }
