2568904f27fdac753c0dbae78c90c067f3a33f18
MnguniMJ <mabalehamnguni@gmail.com>
2025-04-23 00:14:22 +0200

    added middleware to correct google error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,2 @@
 import { handlers } from "@/auth" // Referring to the auth.ts we just created
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
 export const { GET, POST } = handlers
- 
-export const { auth, signIn, signOut } = NextAuth({
-  providers: [
-    Google({
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-  ],
-})
