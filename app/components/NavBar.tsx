59d5dd6f629f866e85a1e5288ccd5dfcc32fd938
MnguniMJ <mabalehamnguni@gmail.com>
2025-04-23 00:35:01 +0200

    nav fix

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex bg-sky-400 p-5 mb-5 ">
+    <div className="flex bg-sky-400 p-5 mb-5 space-x-3">
       <Link href="/" className="mr-5">
         NextJs
       </Link>
