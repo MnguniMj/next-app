6887caa4eecff34e938bceb4c448dd679400adb2
MnguniMJ <mabalehamnguni@gmail.com>
2025-04-29 23:15:00 +0200

    auth and redirecting

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 import React, { Suspense } from "react";
 import UserTable from "./UserTable";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 interface Props {
   searchParams: { sortOrder: string };
 }
 
 const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
+  const session = await auth();
+
+  if (!session) redirect("/api/auth/signin");
+
   return (
     <>
       <h1>Users</h1>
